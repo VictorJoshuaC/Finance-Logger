c24c94e11d7376aaf5eed55029225e8867f75e2b
vicky joshua <vickyjoshua49gmail.com>
2023-09-27 22:48:54 +0000

    added a function in type s

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,7 @@
 "use strict";
+const grate = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
 console.log('testing');
+grate(9, 1, 'true');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,9 @@
+
+ const grate = (a:number, b:number, c?: number | string )=>{
+    console.log(a + b);
+    console.log(c);    
+}
 console.log('testing');
+
+grate(9,1,'true')
 
